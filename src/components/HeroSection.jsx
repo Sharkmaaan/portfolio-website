87e0082717f20c6a7d155924d4c375e3bce58995
Sharkmaaan <sanjay@sanjayghosh.com>
2025-12-29 12:28:35 +0100

    Minor changes in index.html

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { ArrowDown } from "lucide-react";
 export const HeroSection = () => {
     return <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4"
